aa82bf5902ba55dd9aa83a042b42a525f50fd1a0
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-07 19:27:08 -0300

    Convertendo data-hora global para local

---
+++ bin/_6_convertendo_data_hora_para_texto/README.md
@@ -0,0 +1,3 @@
+![image](https://github.com/JoseLeonardoCordeiroBahia/topicos-especiais-data-hora-java/assets/63564226/b8e70c80-b758-4a2b-b93f-def69006410b)
+
+![image](https://github.com/JoseLeonardoCordeiroBahia/topicos-especiais-data-hora-java/assets/63564226/97d6cb61-a330-44ba-8d42-33d65a4b7249)
---
+++ src/_7_convertendo_data_hora_global_para_local/Program.java
@@ -0,0 +1,39 @@
+package _7_convertendo_data_hora_global_para_local;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Instant d03 = Instant.parse("2023-07-20T01:30:26Z");
+		
+		LocalDate r1 = LocalDate.ofInstant(d03, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d03, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d03, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d03, ZoneId.of("Portugal"));
+		
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);
+		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);
+		
+		System.out.println();
+		
+		LocalDate d01 = LocalDate.parse("2023-07-20");
+		
+		System.out.println("d01 dia = " + d01.getDayOfMonth());
+		System.out.println("d01 mês = " + d01.getMonthValue());
+		System.out.println("d01 ano = " + d01.getYear());
+		
+		System.out.println();
+		
+		LocalDateTime d02 = LocalDateTime.parse("2023-07-20T01:30:26");
+		
+		System.out.println("d02 hora = " + d02.getHour());
+		System.out.println("d02 minuto = " + d02.getMinute());
+	}
+	
+}
